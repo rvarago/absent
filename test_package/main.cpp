cabde96ac56cee9c21a68ec46e7c5a8e6c941dec
Rafael Varago <rafael.varago@egym.de>
2019-05-31 22:08:03 +0200

    conan: Add support to create a package for absent via Conan
    The command:
    
    make conan-package
    
    Will build a conan package and deploy it to the local cache.

---
+++ test_package/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <optional>
+#include "absent/absent.h"
+
+using namespace rvarago::absent;
+
+int main() {
+    auto constexpr maybe_an_approximate_answer = std::optional{41};
+    auto constexpr exact_answer = (maybe_an_approximate_answer | [](auto const& el) { return el + 1; }).value();
+    static_assert(42 == exact_answer);
+    std::cout << "rvarago::absent works! The exact answer is: " << exact_answer << '\n';
+    return 0;
+}
