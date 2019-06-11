c81b4e34c81bb8d28cd15fbfe8da44bd5456f884
Rafael Varago <rafael.varago@egym.de>
2019-06-11 14:59:03 +0200

    cleanup: Simplify implementation in the tests for absent_tests

--- tests/absent_tests.cpp
+++ tests/absent_tests.cpp
@@ -7,51 +7,53 @@
 
 using namespace rvarago::absent;
 
-TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCode_when_allAreNotEmpty_shouldReturnTheZipCode) {
-    struct person{};
-    struct address{};
+namespace {
 
-    auto const find_person = []{ return std::optional{person{}}; };
-    auto const find_address = [](auto const&){ return std::optional{address{}}; };
-    auto const zip_code = [](auto const&){return "123";};
+    struct person final {};
+    struct address final {};
 
-    EXPECT_EQ(std::optional{"123"}, find_person() >> find_address | zip_code);
-}
+    TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCode_when_allAreNotEmpty_shouldReturnTheZipCode) {
+        auto const find_person = [] { return std::optional{person{}}; };
+        auto const find_address = [](auto const &) { return std::optional{address{}}; };
+        auto const zip_code = [](auto const &) { return 42; };
 
-TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCode_when_anyIsEmpty_shouldReturnAnEmptyZipCode) {
-    struct person{};
-    struct address{};
+        EXPECT_EQ(std::optional{42}, find_person() >> find_address | zip_code);
+    }
 
-    auto const find_person = []{ return std::optional{person{}}; };
-    auto const find_address = [](auto const&){ return std::optional{address{}}; };
+    TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCode_when_anyIsEmpty_shouldReturnAnEmptyZipCode) {
+        auto const find_person = [] { return std::optional{person{}}; };
+        auto const find_address = [](auto const &) { return std::optional{address{}}; };
 
-    auto const find_person_empty = []() -> std::optional<person> { return std::nullopt; };
-    auto const find_address_empty = [](auto const&) -> std::optional<address> { return std::nullopt; };
+        auto const find_person_empty = []() -> std::optional<person> { return std::nullopt; };
+        auto const find_address_empty = [](auto const &) -> std::optional<address> { return std::nullopt; };
 
-    auto const zip_code = [](auto const&){return "123";};
+        auto const zip_code = [](auto const &) { return 42; };
 
-    EXPECT_FALSE(find_person() >> find_address_empty | zip_code);
-    EXPECT_FALSE(find_person_empty() >> find_address | zip_code);
-    EXPECT_FALSE(find_person_empty() >> find_address_empty | zip_code);
-}
+        EXPECT_FALSE(find_person() >> find_address_empty | zip_code);
+        EXPECT_FALSE(find_person_empty() >> find_address | zip_code);
+        EXPECT_FALSE(find_person_empty() >> find_address_empty | zip_code);
+    }
 
-TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCodeAsMemberFunction_when_allAreNotEmpty_shouldReturnTheZipCode) {
-    struct address{
-        std::string zip_code() const {return "123";}
-    };
+    TEST(combine_bind_fmap, given_anHierarchyOfPersonAddressAndZipCodeAsMemberFunction_when_allAreNotEmpty_shouldReturnTheZipCode) {
 
-    struct person{
-        std::optional<address> find_address() const { return address{}; }
-        std::optional<address> find_address_empty() const { return std::nullopt; }
-    };
+        struct address {
+            int zip_code() const { return 42; }
+        };
 
-    auto const find_person = []{ return std::optional{person{}}; };
-    auto const find_person_empty = []{ return std::optional<person>{}; };
+        struct person {
+            std::optional<address> find_address() const { return address{}; }
+            std::optional<address> find_address_empty() const { return std::nullopt; }
+        };
 
-    EXPECT_EQ(std::optional{"123"}, find_person() >> &person::find_address | &address::zip_code);
-    EXPECT_FALSE(find_person() >> &person::find_address_empty | &address::zip_code);
-    EXPECT_FALSE(find_person_empty() >> &person::find_address | &address::zip_code);
-    EXPECT_FALSE(find_person_empty() >> &person::find_address_empty | &address::zip_code);
+        auto const find_person = [] { return std::optional{person{}}; };
+        auto const find_person_empty = [] { return std::optional<person>{}; };
+
+        EXPECT_EQ(std::optional{42}, find_person() >> &person::find_address | &address::zip_code);
+        EXPECT_FALSE(find_person() >> &person::find_address_empty | &address::zip_code);
+        EXPECT_FALSE(find_person_empty() >> &person::find_address | &address::zip_code);
+        EXPECT_FALSE(find_person_empty() >> &person::find_address_empty | &address::zip_code);
+    }
+
 }
 
 int main(int argc, char **argv) {
