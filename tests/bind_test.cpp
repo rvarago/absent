8077ed6f5a881b47d41dbec9177606fbb8e211de
Rafael Varago <rafael.varago@egym.de>
2019-06-11 15:00:57 +0200

    cleanup: Use brace-init when instantiating an std::optional

--- tests/bind_test.cpp
+++ tests/bind_test.cpp
@@ -21,9 +21,9 @@ TEST(bind, given_AnOptional_when_NotEmpty_should_ReturnNewOptionalWithTheMappedV
 
     std::optional<int> const some_zero{0};
 
-    EXPECT_EQ(std::optional(1), some_zero >> maybe_increment);
-    EXPECT_EQ(std::optional(2), some_zero >> maybe_increment >> maybe_increment);
-    EXPECT_EQ(std::optional(3), some_zero >> maybe_increment >> maybe_increment >> maybe_increment);
+    EXPECT_EQ(std::optional{1}, some_zero >> maybe_increment);
+    EXPECT_EQ(std::optional{2}, some_zero >> maybe_increment >> maybe_increment);
+    EXPECT_EQ(std::optional{3}, some_zero >> maybe_increment >> maybe_increment >> maybe_increment);
 }
 
 TEST(bind, given_AnOptional_when_NotEmptyAndMappedToANewType_should_ReturnNewOptionalWithTheMappedValueOfTheNewType) {
@@ -32,8 +32,8 @@ TEST(bind, given_AnOptional_when_NotEmptyAndMappedToANewType_should_ReturnNewOpt
 
     std::optional<std::string> const some_zero_as_string{"0"};
 
-    EXPECT_EQ(std::optional(0), some_zero_as_string >> maybe_string_to_int);
-    EXPECT_EQ(std::optional("0"), some_zero_as_string >> maybe_string_to_int >> maybe_int_to_string);
+    EXPECT_EQ(std::optional{0}, some_zero_as_string >> maybe_string_to_int);
+    EXPECT_EQ(std::optional{"0"}, some_zero_as_string >> maybe_string_to_int >> maybe_int_to_string);
 }
 
 TEST(bind, given_AnOptional_when_NotEmptyAndThenEmpty_should_ReturnAnEmptyOptional) {
