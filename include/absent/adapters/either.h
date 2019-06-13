85d9775391ec3f367fb6547577e0c426d3cc7f33
Rafael Varago <rafael.varago@egym.de>
2019-06-13 11:21:41 +0200

    change: Move either to ns rvarago::absent::adapters

--- include/absent/adapters/either.h
+++ include/absent/adapters/either.h
@@ -3,54 +3,59 @@
 
 #include <variant>
 
-namespace rvarago::absent::nullable {
+namespace rvarago::absent {
 
-    template<typename A, typename E>
-    using either = std::variant<A, E>;
-
-    namespace syntax {
-
-        template <typename A, typename E>
-        struct empty<std::variant, A, E> final {
-
-            static constexpr auto _(std::variant<A, E> const& input) -> bool {
-                return !std::holds_alternative<A>(input);
-            }
-
-        };
-
-        template <typename A, typename E>
-        struct value<std::variant, A, E> final {
-
-            static constexpr auto _(std::variant<A, E> input) -> A {
-                return std::get<A>(input);
-            }
-
-        };
-
-        template <typename A, typename B, typename E>
-        struct make_empty<std::variant<A, E>, std::variant<B, E>> final {
-
-            static constexpr auto _(std::variant<A, E> input) -> std::variant<B, E> {
-                return std::variant<B, E>{std::get<E>(input)};
-            }
-
-        };
+    namespace adapters {
+        template<typename A, typename E>
+        using either = std::variant<A, E>;
     }
 
+    namespace nullable {
 
-    namespace instances {
+        namespace syntax {
 
-        template<typename Mapper, typename A, typename E>
-        struct fmapper<std::variant, Mapper, A, E> final {
+            template<typename A, typename E>
+            struct empty<std::variant, A, E> final {
 
-            static constexpr decltype(auto) _(std::variant<A, E> input, Mapper fn) {
-                using Result = std::variant<decltype(fn(std::declval<A>())), E>;
-                auto const flat_mapper = [&fn](auto value) { return Result{fn(std::move(value))}; };
-                return binder<std::variant, decltype(flat_mapper), A, E>::_(std::move(input), flat_mapper);
-            }
-        };
+                static constexpr auto _(std::variant<A, E> const &input) -> bool {
+                    return !std::holds_alternative<A>(input);
+                }
 
+            };
+
+            template<typename A, typename E>
+            struct value<std::variant, A, E> final {
+
+                static constexpr auto _(std::variant<A, E> input) -> A {
+                    return std::get<A>(input);
+                }
+
+            };
+
+            template<typename A, typename B, typename E>
+            struct make_empty<std::variant<A, E>, std::variant<B, E>> final {
+
+                static constexpr auto _(std::variant<A, E> input) -> std::variant<B, E> {
+                    return std::variant<B, E>{std::get<E>(input)};
+                }
+
+            };
+        }
+
+
+        namespace instances {
+
+            template<typename Mapper, typename A, typename E>
+            struct fmapper<std::variant, Mapper, A, E> final {
+
+                static constexpr decltype(auto) _(std::variant<A, E> input, Mapper fn) {
+                    using Result = std::variant<decltype(fn(std::declval<A>())), E>;
+                    auto const flat_mapper = [&fn](auto value) { return Result{fn(std::move(value))}; };
+                    return binder<std::variant, decltype(flat_mapper), A, E>::_(std::move(input), flat_mapper);
+                }
+            };
+
+        }
     }
 }
 
