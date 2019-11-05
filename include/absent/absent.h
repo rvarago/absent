e20b6c1ffd51bf137d4cfdeb12f7e669ec4e2c22
Rafael Varago <varago.rafael@gmail.com>
2019-11-05 21:23:14 +0100

    Add new combinator: attempt
    That wraps code that throws exceptions into composable nullables.

--- include/absent/absent.h
+++ include/absent/absent.h
@@ -1,6 +1,7 @@
 #ifndef RVARAGO_ABSENT_ABSENT_H
 #define RVARAGO_ABSENT_ABSENT_H
 
+#include "absent/combinators/attempt.h"
 #include "absent/combinators/bind.h"
 #include "absent/combinators/eval.h"
 #include "absent/combinators/fmap.h"
---
+++ include/absent/combinators/attempt.h
@@ -0,0 +1,28 @@
+#ifndef RVARAGO_ABSENT_ATTEMPT_H
+#define RVARAGO_ABSENT_ATTEMPT_H
+
+#include <exception>
+
+#include "absent/nullable/syntax.h"
+
+namespace rvarago::absent {
+
+    template <template <typename...> typename Nullable, typename... Rest>
+    struct attempt {
+
+        template <typename BaseException = std::exception, typename NullaryFunction>
+        static auto run(NullaryFunction unsafe) -> Nullable<decltype(unsafe()), Rest...> {
+            using namespace nullable::syntax;
+            using ValueT = decltype(unsafe());
+            try {
+                return make<Nullable, ValueT, Rest...>::_(unsafe());
+            }
+            catch (BaseException const& ex) {
+                return make_empty<Nullable<ValueT, Rest...>>::_(ex);
+            }
+        }
+    };
+
+}
+
+#endif
