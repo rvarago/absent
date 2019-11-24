42b5a646d8308dfafea3177fbf2d5f85b8f0f3dc
Rafael Varago <varago.rafael@gmail.com>
2019-11-24 15:31:08 +0100

    change: Rename attempt::run to attempt::or_catch
    To better reflect its purpose.

--- tests/attempt_test.cpp
+++ tests/attempt_test.cpp
@@ -19,14 +19,14 @@ SCENARIO("attempt provides a generic and type-safe way to wrap an exception into
 
             AND_WHEN("exception is of expected type") {
                 THEN("return a new empty nullable") {
-                    auto const empty = attempt<std::optional>::run(throw_runtime_error);
+                    auto const empty = attempt<std::optional>::or_catch(throw_runtime_error);
                     CHECK(empty == std::nullopt);
                 }
             }
 
             AND_WHEN("exception is not of expected type") {
                 THEN("propagate the exception previously thrown") {
-                    CHECK_THROWS_AS(attempt<std::optional>::run<std::logic_error>(throw_runtime_error),
+                    CHECK_THROWS_AS(attempt<std::optional>::or_catch<std::logic_error>(throw_runtime_error),
                                     std::runtime_error);
                 }
             }
@@ -37,7 +37,7 @@ SCENARIO("attempt provides a generic and type-safe way to wrap an exception into
             auto never_throw = []() -> int { return 42; };
 
             THEN("return the result inside a non-empty nullable") {
-                auto const success = attempt<std::optional>::run(never_throw);
+                auto const success = attempt<std::optional>::or_catch(never_throw);
                 CHECK(success == std::optional{42});
             }
         }
