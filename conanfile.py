f2a66ae00a6b73cc5a1b0f697759e8490bf7653a
Rafael Varago <varago.rafael@gmail.com>
2020-04-05 10:30:32 +0200

    conan: Remove unused packaging functions
    They are leftovers from the moving to conan-center.

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,5 @@
 from conans import ConanFile
 
 class AbsentConan(ConanFile):
-    name        = "absent"
-
-    description = "A simple library to compose nullable types in a declarative style for the modern C++ programmer"
-    author      = "Rafael Varago (rvarago)"
-    topics      = ("nullable-type", "composition", "monadic-interface", "declarative-programming")
-    license     = "MIT"
-    homepage    = "https://github.com/rvarago/absent"
-    url         = homepage
-    exports     = ["README.md", "LICENSE"]
-
     build_requires  = "catch2/2.11.0"
     generators      = "cmake_find_package"
-
-    exports_sources = "include/*"
-    no_copy_source = True
-
-    def package(self):
-        self.copy("*.h")
-
-    def package_id(self):
-        self.info.header_only()
