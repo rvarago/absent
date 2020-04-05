081f0f72ac4f0c9b801fce69edfcf3730d0a9654
Rafael Varago <varago.rafael@gmail.com>
2020-04-05 10:36:46 +0200

    deps: Upgrade catch2 to 2.11.3

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile
 
 class AbsentConan(ConanFile):
-    build_requires  = "catch2/2.11.0"
+    build_requires  = "catch2/2.11.3"
     generators      = "cmake_find_package"
